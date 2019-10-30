ff890ff775f5c2e018cd07f2e67a8ab2ec5ecdd6
prakhartu <prakharpg.83@gmail.com>
2019-10-30 22:13:52 +0530

    Updates

---
+++ precache-manifest.1f8e2767c12e7510e2aa3d8bbc6cddcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c19cdb8e814bc362db75b77c067a79f3",
+    "url": "/PrakharTU/reactappui/index.html"
+  },
+  {
+    "revision": "04dcaad53a556cc7026b",
+    "url": "/PrakharTU/reactappui/static/css/main.f5d6b251.chunk.css"
+  },
+  {
+    "revision": "857ed0a9d6c6b936e5f8",
+    "url": "/PrakharTU/reactappui/static/js/2.184cb4ec.chunk.js"
+  },
+  {
+    "revision": "04dcaad53a556cc7026b",
+    "url": "/PrakharTU/reactappui/static/js/main.c5d042ff.chunk.js"
+  },
+  {
+    "revision": "a70124c412b3e058e308",
+    "url": "/PrakharTU/reactappui/static/js/runtime-main.4397fe78.js"
+  },
+  {
+    "revision": "e8397b8d8e1f745cd906240f9f80e13d",
+    "url": "/PrakharTU/reactappui/static/media/mltoolicon.e8397b8d.png"
+  }
+]);
